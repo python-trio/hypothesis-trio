2bb4b372be1df603cafa8da019f8fd58ae112d76
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-06-26 13:12:07 +0200

    Replace deprecated use of GenericStateMachine by RuleBaseStateMachine in tests

--- hypothesis_trio/_tests/test_stateful.py
+++ hypothesis_trio/_tests/test_stateful.py
@@ -1,22 +1,25 @@
 from hypothesis_trio.stateful import *  # Import to trigger monkeypatch
-from hypothesis.stateful import GenericStateMachine, run_state_machine_as_test
+from hypothesis.stateful import RuleBasedStateMachine, run_state_machine_as_test, initialize, rule, invariant
 from hypothesis.strategies import just
 
 
 def test_vanilla_stateful_with_monkeypatch():
-    class LogEventsStateMachine(GenericStateMachine):
+    class LogEventsStateMachine(RuleBasedStateMachine):
         events = []
+
+        @initialize()
+        def init(self):
+            self.events.append('init')
 
         def teardown(self):
             self.events.append('teardown')
 
-        def steps(self):
-            return just(42)
-
-        def execute_step(self, step):
-            assert step is 42
+        @rule(arg=just(42))
+        def step(self, arg):
+            assert arg is 42
             self.events.append('execute_step')
 
+        @invariant()
         def check_invariants(self):
             self.events.append('check_invariants')
 
@@ -31,8 +34,8 @@ def test_vanilla_stateful_with_monkeypatch():
             current_run_events = []
 
     for run_events in per_run_events:
-        expected_events = ['check_invariants']
+        expected_events = ['check_invariants', 'init', 'check_invariants']
         expected_events += ['execute_step', 'check_invariants'
-                            ] * ((len(run_events) - 2) // 2)
+                            ] * ((len(run_events) - 4) // 2)
         expected_events.append('teardown')
         assert run_events == expected_events
